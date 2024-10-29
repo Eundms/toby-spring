5c3d4c3ac9486b7ea8968e9cea71084bea8b58af
Eundms <dms011221@gmail.com>
2024-10-29 19:27:05 +0900

    DataAccessException

--- hellospring/src/main/java/tobyspring/hellospring/DataClient.java
+++ hellospring/src/main/java/tobyspring/hellospring/DataClient.java
@@ -4,6 +4,9 @@ import java.math.BigDecimal;
 
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import tobyspring.hellospring.data.OrderRepository;
 import tobyspring.hellospring.order.Order;
@@ -12,8 +15,21 @@ public class DataClient {
 	public static void main(String[] args) {
 		BeanFactory beanFactory = new AnnotationConfigApplicationContext(DataConfig.class);
 		OrderRepository orderRepository = beanFactory.getBean(OrderRepository.class);
-		Order order = new Order("2",BigDecimal.TEN);
-		orderRepository.save(order);
+		JpaTransactionManager transactionManager = beanFactory.getBean(JpaTransactionManager.class);
+		try {
+			new TransactionTemplate(transactionManager).execute(status -> {
+				// transcation begin
+				Order order = new Order("2", BigDecimal.TEN);
+				orderRepository.save(order);
+
+				Order order2 = new Order("2", BigDecimal.TWO);
+				orderRepository.save(order2);
+				// commit
+				return null;
+			});
+		} catch (DataIntegrityViolationException e) {
+			System.out.println("주문번호 중복 복구 작업");
+		}
 
 	}
 }
